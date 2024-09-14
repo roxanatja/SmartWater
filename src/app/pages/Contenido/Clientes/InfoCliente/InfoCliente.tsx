5874528d5799c82be244fe06a0980825045bbac1
XJuanCarlosXD <juancarlos192003@gmail.com>
2024-09-14 13:58:16 -0400

    update SmartWaterApi.js and InfoCliente.tsx

--- src/app/pages/Contenido/Clientes/InfoCliente/InfoCliente.tsx
+++ src/app/pages/Contenido/Clientes/InfoCliente/InfoCliente.tsx
@@ -19,7 +19,7 @@ const InfoCliente = (client: Client) => {
   const [date, setDate] = useState<string>();
   const [showCobroPopUp, setShowCobroPopUp] = useState<boolean>(false); // Agregado el estado para el Pop-Up
   const location = client.location;
-  const url = `https://www.google.com/maps/place/${location.latitude} ${location.longitude}`;
+  const url = `https://www.google.com/maps/place/${location?.latitude} ${location?.longitude}`;
 
   useEffect(() => {
     const getZone = async () => {
