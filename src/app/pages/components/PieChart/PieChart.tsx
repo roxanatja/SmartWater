9a1557ea0c90edd9708c2396f90062b4aecafb26
danny1998cuba <danny.glezcuet98@gmail.com>
2025-01-31 19:05:52 -0500

    Reports loans/sales & sales/product

--- src/app/pages/components/PieChart/PieChart.tsx
+++ src/app/pages/components/PieChart/PieChart.tsx
@@ -1,41 +1,59 @@
 import React from 'react';
 import { Pie } from 'react-chartjs-2';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, ChartData } from 'chart.js';
 import "./PieChart.css";
+import ChartDataLabels from "chartjs-plugin-datalabels";
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(ChartDataLabels, ArcElement, Tooltip, Legend);
 
-var options = {
-    responsive: true,
-    maintainAspectRatio: false,
-};
-
-var beneficios = [70];
-var vendidos = [30];
-
-
-var data = {
-    labels: [],
+const data: ChartData<'pie', number[], string> = {
+    labels: ["Ventas", "Prestamos"],
     datasets: [
         {
-            data: [vendidos, beneficios],
+            data: [30, 70],
             backgroundColor: ['#1A3D7D', '#367DFD'],
             //barThickness: 73,
             borderColor: "#FFF",
-            borderWidth: 2,
+            borderWidth: 2
         },
     ]
 }
 
-const PieChart = () => { 
+const PieChart = () => {
 
-    return(
+    return (
         <>
-            <div className='PieChart-container'>
-                <Pie data={data} options={options} />
+            <div className='w-full'>
+                <Pie data={data} options={{
+                    responsive: true,
+                    maintainAspectRatio: false,
+                    font: { family: "Poppins" },
+                    plugins: {
+                        legend: {
+                            labels: {
+                                color: document.body.classList.contains('dark') ? "#fefefe" : "#1B1B1B",
+                                font: { family: "Poppins", size: 16 },
+
+                            },
+                            display: true,
+                            align: 'start',
+                            position: "bottom"
+                        },
+                        datalabels: {
+                            font: { family: "Poppins" },
+                            anchor: 'center',
+                            align: 'top',
+                            color: "#fefefe",
+                        },
+                        tooltip: {
+                            titleFont: { family: "Poppins" },
+                            bodyFont: { family: "Poppins" }
+                        }
+                    }
+                }} />
             </div>
         </>
     )
 }
 
-export{PieChart}
+export { PieChart }
