93df9e213bf909f4b8761f4026f57dfdbba1efed
danny1998cuba <danny.glezcuet98@gmail.com>
2025-02-13 18:24:03 -0500

    Map providers

--- src/app/pages/components/LeafletMap/LeafletMap.tsx
+++ src/app/pages/components/LeafletMap/LeafletMap.tsx
@@ -94,6 +94,8 @@ const LeafletMap = ({ clients, onAdd, activeClient, latitude, longitude }: MapPr
                     {
                         mapType === 'roadmap' &&
                         <TileLayer
+                            minZoom={2}
+                            maxZoom={17}
                             attribution={`&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors`}
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         />
@@ -101,19 +103,19 @@ const LeafletMap = ({ clients, onAdd, activeClient, latitude, longitude }: MapPr
                     {
                         mapType === 'satellite' &&
                         <TileLayer
-                            minZoom={0}
-                            maxZoom={20}
-                            attribution={`&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors`}
-                            url="https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.png"
+                            minZoom={2}
+                            maxZoom={17}
+                            attribution={`Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community`}
+                            url="https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
                         />
                     }
                     {
                         mapType === 'terrain' &&
                         <TileLayer
-                            minZoom={0}
-                            maxZoom={18}
-                            attribution={`'&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors`}
-                            url="https://tiles.stadiamaps.com/tiles/stamen_terrain/{z}/{x}/{y}{r}.png"
+                            minZoom={2}
+                            maxZoom={17}
+                            attribution={`Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012`}
+                            url="https://services.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}"
                         />
                     }
 
